6a649a7ba2457470e45c4dfc34c89983fda09db8
Abdullah2elsman <abdullahelsman294@gmail.com>
2026-05-05 05:40:07 +0300

    converting view to class based

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts', views.post_list, name='posts'),
-    path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('posts/<slug:slug>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<slug:slug>/delete/', views.post_delete, name='post_delete'),
+    path('', views.StartingPageView.as_view(), name='index'),
+    path('posts', views.PostListView.as_view(), name='posts'),
+    path('posts/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import get_object_or_404, render        
-
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model =Post
+    ordering = ['-date']
+    context_object_name = 'posts'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        date = queryset[:3]
+        return date
 
-def index(request):
-    lasted_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, 'blog/index.html',{
-        'posts': lasted_posts
-    })
+class PostListView(ListView):
+    template_name = 'blog/all-posts.html'
+    model =Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
 
-def post_list(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/all-posts.html', {
-        'all_posts': all_posts
-    })
+class PostDetailView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html', {
-        'post': identified_post,
-        'post_tags': identified_post.tags.all()
-    })
-
-def post_edit(request, slug):
-    pass
-
-def post_delete(request, slug):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
